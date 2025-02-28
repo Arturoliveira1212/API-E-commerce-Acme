7c50e0c8725cd42c7e39d5374e4265ae6c80306b
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2025-02-28 19:31:50 -0300

    Fix: Corrige ordem de execução dos middlwares.

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@ use Slim\Middleware\BodyParsingMiddleware;
 
 $app = AppFactory::create();
 
-$app->add( new BodyParsingMiddleware() );
 $app->add( new SanitizacaoDadosMiddleware() );
+$app->add( new BodyParsingMiddleware() );
 
 $errorMiddleware = $app->addErrorMiddleware( true, true, true );
 $errorMiddleware->setDefaultErrorHandler( new ErrorHandlerMiddleware() );
