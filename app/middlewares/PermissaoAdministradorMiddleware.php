2aa1a5ce2f9b73e2c848eae2696e5d34f12d79b5
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2025-02-27 21:48:36 -0300

    Feat: Adiciona Middleware para analisar permissão do administrador para executar as rotas.

---
+++ app/middlewares/PermissaoAdministradorMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+namespace app\middlewares;
+
+use app\classes\Administrador;
+use app\classes\factory\ClassFactory;
+use Slim\Psr7\Response;
+use app\classes\jwt\PayloadJWT;
+use app\classes\http\RespostaHttp;
+use app\classes\http\HttpStatusCode;
+use app\services\AdministradorService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class PermissaoAdministradorMiddleware {
+    private $permissoesNecessarias;
+
+    public function __construct( array $permissoesNecessarias ){
+        $this->permissoesNecessarias = $permissoesNecessarias;
+    }
+
+    public function __invoke( ServerRequestInterface $request, RequestHandlerInterface $handler ): ResponseInterface {
+        /** @var PayloadJWT */
+        $payloadJWT = $request->getAttribute('payloadJWT');
+
+        /** @var AdministradorService */
+        $administradorService = ClassFactory::makeService( Administrador::class );
+        $administrador = $administradorService->obterComId( $payloadJWT->sub() );
+        if( ! $administrador instanceof Administrador ){
+            return RespostaHttp::enviarResposta( new Response(), HttpStatusCode::FORBIDDEN, [
+                'erro' => 'Você não tem permissão para realizar essa ação.'
+            ] );
+        }
+
+        foreach( $this->permissoesNecessarias as $permissao ){
+            if( ! $administrador->possuiPermissao( $permissao ) ){
+                return RespostaHttp::enviarResposta( new Response(), HttpStatusCode::FORBIDDEN, [
+                    'erro' => 'Você não tem permissão para realizar essa ação.'
+                ] );
+            }
+        }
+
+        return $handler->handle( $request );
+    }
+}
