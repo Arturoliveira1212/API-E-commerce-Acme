256b23d19ef5f0802c6fa8acb130487d8603e1c5
Arturoliveira1212 <arturalvesdeoliveira28@gmail.com>
2025-03-23 14:32:38 -0300

    Test: Middleware responsável por tratar o corpo requisição.

--- spec/unitarios/middlewares/CorpoRequisicaoMiddleware.spec.php
+++ spec/unitarios/middlewares/CorpoRequisicaoMiddleware.spec.php
@@ -57,10 +57,26 @@ describe( 'CorpoRequisicaoMiddleware', function() {
     });
 
     it( 'Retorna erro(400) quando o corpo de requisição tem os campos obrigatórios mas de tipos inválidos.', function(){
-        $middleware = new CorpoRequisicaoMiddleware( 'application/json', [ 'nome' => 'string' ] );
+        $middleware = new CorpoRequisicaoMiddleware( 'application/json', [
+            'nome' => 'string',
+            'idade' => 'numeric',
+            'quantidade' => 'int',
+            'peso' => 'float',
+            'efetivado' => 'bool',
+            'dias' => 'array',
+            'objeto' => 'object'
+        ] );
 
         $this->request->shouldReceive('getHeaderLine')->with('Content-Type')->andReturn('application/json');
-        $this->request->shouldReceive('getParsedBody')->andReturn( [ 'nome' => 11111 ] );
+        $this->request->shouldReceive('getParsedBody')->andReturn( [
+            'nome' => 11111,
+            'idade' => 'aaaa',
+            'quantidade' => 2.0,
+            'peso' => 2,
+            'efetivado' => 'aaaaa',
+            'dias' => 'array',
+            'objeto' => 'a'
+        ] );
         $response = $middleware( $this->request, $this->handler );
 
         validarErroMiddleware( $response, HttpStatusCode::BAD_REQUEST, [
@@ -68,7 +84,13 @@ describe( 'CorpoRequisicaoMiddleware', function() {
             'message' => 'O corpo da requisição é inválido.',
             'data' => [
                 'erros' => [
-                    'nome' => 'Campo nome deve ser do tipo string.'
+                    'nome' => 'Campo nome deve ser do tipo string.',
+                    'idade' => 'Campo idade deve ser do tipo numeric.',
+                    'quantidade' => 'Campo quantidade deve ser do tipo int.',
+                    'peso' => 'Campo peso deve ser do tipo float.',
+                    'efetivado' => 'Campo efetivado deve ser do tipo bool.',
+                    'dias' => 'Campo dias deve ser do tipo array.',
+                    'objeto' => 'Campo objeto deve ser do tipo object.'
                 ]
             ]
         ] );
@@ -81,6 +103,20 @@ describe( 'CorpoRequisicaoMiddleware', function() {
         $this->request->shouldReceive('getParsedBody')->andReturn( [ 'nome' => 'Artur Alves' ] );
 
         $this->handler->shouldReceive('handle')->with( $this->request )->andReturn( new Response() );
+        $this->request->shouldReceive('withParsedBody')->with( [ 'nome' => 'Artur Alves' ] )->andReturn( $this->request );
+
+        $response = $middleware( $this->request, $this->handler );
+        expect( $response->getStatusCode() )->toEqual( HttpStatusCode::OK );
+    });
+
+    it( 'Deve limpar o corpo requisição e continuar a execução quando outros campos são enviados', function(){
+        $middleware = new CorpoRequisicaoMiddleware( 'application/json', [ 'nome' => 'string' ] );
+
+        $this->request->shouldReceive('getHeaderLine')->with('Content-Type')->andReturn('application/json');
+        $this->request->shouldReceive('getParsedBody')->andReturn( [ 'nome' => 'Artur Alves', 'id' => 1, 'admin' => true ] );
+
+        $this->handler->shouldReceive('handle')->with( $this->request )->andReturn( new Response() );
+        $this->request->shouldReceive('withParsedBody')->with( [ 'nome' => 'Artur Alves' ] )->andReturn( $this->request );
 
         $response = $middleware( $this->request, $this->handler );
         expect( $response->getStatusCode() )->toEqual( HttpStatusCode::OK );
